cde2f1f437b54b3a70c48ac4db8968af5091a3a4
tark salah <tarksalah@gmail.com>
2022-01-09 00:02:51 +0200

    Task 5 first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from scipy import signal as sig
+import math 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+	
+# @app.route('/upload', methods = ['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+
+#     return render_template('index.html')
+        
+@app.route('/plot', methods = ['GET', 'POST'])
+def digital_filter_data():
+    Xaxis_data = pd.read_csv("file1.csv").iloc[:, 0].tolist()
+    Yaxis_data = pd.read_csv("file1.csv").iloc[:, 1].tolist()
+    Filtered_data = []
+    zeros = []
+    poles = []
+    zero = 0
+    pole = 0
+    if request.method == 'POST':
+        data = request.json
+        for index in range(len(data)):
+            if data[index]['type'] == "nonConjZero" or data[index]['type'] == "ConjZero":
+                zero = complex(data[index]['point'][0],data[index]['point'][1])
+                zeros.append(zero)
+
+        for index in range(len(data)):
+            if data[index]['type'] == "nonConjPole" or data[index]['type'] == "ConjPole":
+                pole = complex(data[index]['point'][0],data[index]['point'][1])
+                poles.append(pole)
+
+    sysDnum, sysDden = sig.zpk2tf(zeros, poles, 1)
+    print(sysDnum)
+    print(sysDden)
+    filtered_data_withComplex = sig.lfilter(sysDnum, sysDden, Yaxis_data)
+    print(filtered_data_withComplex)
+    for index in range(len(filtered_data_withComplex)):
+        absolute = abs(filtered_data_withComplex[index])
+        Filtered_data.append(absolute)
+
+    data = jsonify({"data_Xaxis": Xaxis_data, "non_Filtered_Data" : Yaxis_data, "Filtered_Data" : Filtered_data})
+
+    return data
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
---
+++ static/js/Ztransform.js
@@ -0,0 +1,86 @@
+class ZTransform {
+    constructor() {
+        this.MAX_POINTS = 180;
+        this.semiUnitCircle = this.generateSemiUnitCircle();
+    }
+
+    get frequencies() {
+        return this.theta;
+    }
+
+    generateSemiUnitCircle() {
+        this.theta = this.linspace(0, Math.PI, this.MAX_POINTS);
+        let points = [];
+        let x, y;
+        for (let i = 0; i < this.MAX_POINTS; i++) {
+            x = Math.cos(this.theta[i]);
+            y = Math.sin(this.theta[i]);
+            points[i] = [x, y]
+        }
+        return points;
+    }
+
+    difference(point1 = [], point2 = []) {
+        return [point2[0] - point1[0], point2[1] - point1[1]];
+    }
+
+    magnitude(point) {
+        return Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
+    }
+
+    phase(point) {
+        if (point[0] == 0) {
+            return Math.PI / 2;
+        }
+        else {
+            return - Math.atan(point[1] / point[0]);
+        }
+    }
+
+    filter(poles = [[]], zeroes = [[]], allPass = [[]]) {
+        let magResponse = []
+        let phaseResponse = []
+        let allPassPhaseResponse = []
+        let magNum, magDenum, phaseNum, phaseDenum, allPassPhaseNum, allPassPhaseDenum, diff;
+        for (const point of this.semiUnitCircle) {
+            magNum = 1;
+            magDenum = 1;
+            phaseNum = 0;
+            phaseDenum = 0;
+            allPassPhaseNum = 0;
+            allPassPhaseDenum = 0;
+            for (const zero of zeroes) {
+                diff = this.difference(point, zero);
+                magNum = magNum * this.magnitude(diff);
+                phaseNum = phaseNum + this.phase(diff);
+            }
+            for (const pole of poles) {
+                diff = this.difference(point, pole);
+                magDenum = magDenum * this.magnitude(diff);
+                phaseDenum = phaseDenum + this.phase(diff);
+            }
+            for (const a of allPass) {
+                diff = this.difference(point, a);
+                allPassPhaseNum = allPassPhaseNum + this.phase([1 - point[0] * a[0] - point[1] * a[1], point[0] * a[1] - point[1] * a[0]]);
+                allPassPhaseDenum = allPassPhaseDenum + this.phase(diff);
+            }
+            magResponse.push((magNum / magDenum).toFixed(5));
+            phaseResponse.push((phaseNum + allPassPhaseNum - phaseDenum - allPassPhaseDenum).toFixed(5));
+            allPassPhaseResponse.push((allPassPhaseNum - allPassPhaseDenum).toFixed(5));
+        }
+        return {
+            "magnitude": magResponse,
+            "phase": phaseResponse,
+            "allPassPhase": allPassPhaseResponse
+        };
+    }
+
+    linspace(start, end, num) {
+        const step = (end - start) / (num - 1);
+        let arr = [];
+        for (let i = 0; i < num; i++) {
+            arr[i] = (start + (i * step)).toFixed(2);
+        }
+        return arr;
+    }
+}
